ddd42baa64465bfc3824c298143aa0802fe8e5ea
Tanmay Chachad <tanmay.chachad@bista-it-00192.bistasolutions.com>
2026-02-19 19:38:43 +0530

    [ADD] Added pagination and search feature

--- tanmay_owl_js/static/src/views/partner_loader.js
+++ tanmay_owl_js/static/src/views/partner_loader.js
@@ -11,26 +11,65 @@ export class PartnerLoader extends Component{
     {
         this.orm = useService("orm")
 
+        this.allPartners = [];
+
         this.state = useState(
             {
                 partners : [],
-                loading : false
+                loading : false,
+                search: "",
+                page: 1,
+                limit: 10,
+                total: 0,
             }
         );
+        this.loadPartners();
     }
 
     async loadPartners() {
         this.state.loading = true;
+        const offset = (this.state.page - 1) * this.state.limit;
 
         const partners = await this.orm.searchRead(
             "res.partner",
             [],
             ["name","email"],
-            { limit: 10 }
+            {
+                limit: this.state.limit,
+                offset: offset,
+            }
+            // { limit: 10 }
         );
+        const total = await this.orm.searchCount("res.partner", []);
 
+        this.allPartners = partners;
         this.state.partners = partners;
+        this.state.total = total;
         this.state.loading = false;
+    }
+
+    onSearch(ev) {
+        const value = ev.target.value.toLowerCase();
+        this.state.search = value;
+
+        this.state.partners = this.allPartners.filter(partner =>
+            (partner.name && partner.name.toLowerCase().includes(value)) ||
+            (partner.email && partner.email.toLowerCase().includes(value))
+        );
+    }
+
+    nextPage() {
+        if (this.state.page * this.state.limit < this.state.total) {
+            this.state.page++;
+            this.loadPartners();
+        }
+    }
+
+    prevPage() {
+        if (this.state.page > 1) {
+            this.state.page--;
+            this.loadPartners();
+        }
     }
 }
 registry.category("actions").add(
